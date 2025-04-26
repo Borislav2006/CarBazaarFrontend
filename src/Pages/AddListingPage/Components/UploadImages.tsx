aa52e32885283c3d34c50c9dfdecf0bd435e0ce6
Borislav Ivanov <borislav.project@gmail.com>
2025-04-26 17:41:53 +0300

    feat: add upload image

---
+++ src/Pages/AddListingPage/Components/UploadImages.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import "./UploadImages.css";
+import { IoMdCloseCircle } from "react-icons/io";
+import { ListingImage } from "../../../types/Listings";
+
+type Props = {
+  onImagesChange: (images: File[]) => void;
+  defaultImageUrls?: ListingImage[];
+};
+
+const UploadImages = ({ onImagesChange, defaultImageUrls = [] }: Props) => {
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+  const [existingImages, setExistingImages] = useState<ListingImage[]>([]);
+
+  useEffect(() => {
+    if (defaultImageUrls.length > 0) {
+      setExistingImages(defaultImageUrls);
+    }
+  }, [defaultImageUrls]);
+
+  const onFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files) return;
+
+    const fileArray = Array.from(files);
+    const newFiles = [...uploadedFiles, ...fileArray];
+
+    setUploadedFiles(newFiles);
+    onImagesChange(newFiles);
+  };
+
+  const onNewImageRemove = (index: number) => {
+    const updatedFiles = [...uploadedFiles];
+    updatedFiles.splice(index, 1);
+    setUploadedFiles(updatedFiles);
+    onImagesChange(updatedFiles);
+  };
+
+  return (
+    <div>
+      <h2 className="upload-title">Upload Images</h2>
+      <div className="images-container">
+        {/* Render existing images without delete button */}
+        {existingImages.map((image, index) => (
+          <div key={`existing-${index}`}>
+            <img src={`https://localhost:5001/images/${image.imagePath}`} className="images-upload" />
+          </div>
+        ))}
+
+        {/* Render new uploaded images with delete button */}
+        {uploadedFiles.map((file, index) => (
+          <div key={`uploaded-${index}`}>
+            <IoMdCloseCircle
+              className="close-button"
+              onClick={() => onNewImageRemove(index)}
+            />
+            <img src={URL.createObjectURL(file)} className="images-upload" />
+          </div>
+        ))}
+
+        {/* Upload input */}
+        <label>
+          <div className="images-box">
+            <h2 className="plus">+</h2>
+          </div>
+          <input
+            type="file"
+            multiple
+            onChange={onFileSelected}
+            className="input-images"
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default UploadImages;
